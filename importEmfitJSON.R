0bf61db79b78e4b88cd801824249caf9bc2b5bf4
Crt Ahlin <crt.ahlin@gmail.com>
2014-10-03 12:43:44 +0200

    Removed some test code.

--- importEmfitJSON.R
+++ importEmfitJSON.R
@@ -8,29 +8,9 @@ importEmfitJSON <- function(filename, timezone) {
   
   calc_data[,"timestamp"] <- as.POSIXct(origin="1970-01-01", tz="GMT", x=calc_data[,"timestamp"])
   head(calc_data)
-  # transform times to local format (Ljubljana)
+  # transform times to local format (e.g. Ljubljana timezone)
   calc_data[,"timestamp"] <- as.POSIXct(format(calc_data[,"timestamp"], tz=timezone))
   
   return(calc_data)
   
 }
-
-
-
-
-filename <- "~/Dropbox/DataSets/Emfit/device-10-presence-report-1019.json"
-temp <- importEmfitJSON(filename, timezone="Europe/Ljubljana")
-head(temp)
-tail(temp)
-str(temp)
-temp[1000:1005,]
-summary(temp)
-
-library(ggplot2)
-plot <- ggplot(data = temp) + geom_point(aes(x=timestamp, y=heart_rate), color="black") + theme_bw()
-plot <- plot + geom_point(aes(x=timestamp, y=breathing_rate), color="blue")
-plot <- plot + scale_x_datetime() + guide_legend()
-plot
-attributes(temp)
-class(temp[,"timestamp"])
-# TODO: reshape the dataset with melt() to have "values" and "groups"
